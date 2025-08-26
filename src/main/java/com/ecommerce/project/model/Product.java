9c8b4a49d58f2c04999ce3df50a7a983e0794964
Samarjiit <samarjitmahi6245@gmail.com>
2025-08-26 17:14:24 +0530

    user management tables are created and related _user_roles_address_product

--- src/main/java/com/ecommerce/project/model/Product.java
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -6,11 +6,14 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "products")
+@ToString
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -32,5 +35,10 @@ public class Product {
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    //user will persist in product table with name seller_id
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
 
 }
